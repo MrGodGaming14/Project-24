56fcb4a63bef5c694e7873c17bdfb888afbfc4eb
MrGodGaming14 <73659533+MrGodGaming14@users.noreply.github.com>
2020-12-18 16:00:37 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,21 +6,27 @@ const Body = Matter.Body;
 
 var paper;
 var dustbin1, dustbin2, dustbin3;
+var ground;
 function preload()
 {
 	
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	dustbin1 = new Dustbin(400, 400, 150, );
-	dustbin2 = new Dustbin(200, 400, 150, );
+	dustbin1 = new Dustbin(1325, 635, 10, 100);
+	dustbin2 = new Dustbin(1465, 635, 10, 100);
+	dustbin3 = new Dustbin(1395, 685, 125, 10);
+
+	ground = new Ground(815,height,1900,20)
+
+    //paper = Paper(100, 300, 20);
 
 	Engine.run(engine);
   
@@ -33,7 +39,10 @@ function draw() {
   Engine.update(engine);
 
   dustbin1.display();
- 
+  dustbin2.display();
+  dustbin3.display();
+  ground.display();
+  //paper.display();
 }
 
 
